ae6ff95bc6150fdc6bdd4ad56486a30f0e5ff0ab
mitrikhoury <mitkhoury@gmail.com>
2024-06-09 14:48:21 +0300

    login_commit_1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,7 @@
+
+
+
+
 plugins {
     alias(libs.plugins.androidApplication)
 }
@@ -37,7 +41,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.google.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.volley)
+    implementation(libs.gson)
+
 }
